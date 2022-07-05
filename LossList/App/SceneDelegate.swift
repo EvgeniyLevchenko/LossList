5b6f071fefc7365106fdcdfd4302be27dbe06122
EvgeniyLevchenko <evgeniy.levchnko@gmail.com>
2022-07-05 15:08:21 +0300

    Initial commit

---
+++ LossList/App/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  LossList
+//
+//  Created by QwertY on 03.07.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
+
